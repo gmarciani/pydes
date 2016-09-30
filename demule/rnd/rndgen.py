bddb9d0deb08be87b8a1ebaa266d42b6218e9785
Giacomo Marciani <giacomo.marciani@gmail.com>
2016-09-30 12:08:23 +0200

    random generator as object

--- demule/rnd/rndgen.py
+++ demule/rnd/rndgen.py
@@ -24,7 +24,7 @@ Keep in mind the following table:
 
 _MODULUS = 2147483647
 _MULTIPLIER = 48271
-_ISEED = 1
+_ISEED = 123456789
 _STREAMS = 256
 _JUMPER = 22925
 
@@ -38,121 +38,117 @@ class MarcianiMultiStream(object):
     Utility class to encapsulate rndgen functionalities.
     """
 
-    def __init__(self, iseed=_ISEED):
+    def __init__(self, iseed=_ISEED,
+                 modulus=_MODULUS,
+                 multiplier=_MULTIPLIER,
+                 streams=_STREAMS,
+                 jumper=_JUMPER):
         """
         Creates a new random number generator.
-        :param iseed: (int) the initial seed.
+        :param iseed: (int) the initial seed; must be positive.
+        Default is 123456789.
+        :param modulus: (int) the modulus; must be positive and prime.
+        Default is 2147483647.
+        :param multiplier: (int) the multiplier; must be a FP/MC multiplier of
+        *modulus*.
+        Default is 48271.
+        :param streams: (int) the number of disjoint streams; must be positive.
+        Default is 256.
+        :param jumper: (int) the jumper for the given streams; must be a
+        suitable jumper for the given number of *streams*.
+        Default is 22925.
         """
-        self._seed = iseed
-        select_stream(0)
-        plant_seeds(self._seed)
+        self._iseed = self._seed = iseed
+        self._stream = 0
+        self._modulus = modulus
+        self._multiplier = multiplier
+        self._streams = streams
+        self._jumper = jumper
 
-    def plant_seeds(self, iseed):
+        self._init = False
+
+        self._seeds = [int(self._iseed)] * self._streams
+        self.plant_seeds(self._iseed)
+
+    def plant_seeds(self, x):
         """
         Initializes all the streams of the generator.
-        :param iseed: (int) the initial seed.
+        :param x: (int) the initial seed.
         """
-        self._seed = iseed
-        select_stream(0)
-        plant_seeds(self._seed)
+        #self._seed = x
+        #self.stream(0)
+        #plant_seeds(self._seed)
 
-    def stream(self, stream):
+        Q = int(self._modulus / self._jumper)
+        R = int(self._modulus % self._jumper)
+
+        self._init = True
+        s = self._stream
+        self.stream(0)
+        self.put_seed(x)
+        self._stream = s
+        for j in range(1, self._streams):
+            x = int(self._jumper * (self._seeds[j - 1] % Q) - R * int((self._seeds[j - 1] / Q)))
+            if x > 0:
+                self._seeds[j] = x
+            else:
+                self._seeds[j] = x + self._modulus
+
+    def get_seed(self):
+        """
+        Retrieves the seed of the current stream.
+        :return: (int) the seed of the current stream.
+        """
+        return self._seeds[self._stream]
+
+    def put_seed(self, x):
+        """
+        Initializes the current stream with the specified seed.
+        :param x: (int) the seed.
+        """
+        if x > 0:
+            x %= self._modulus
+        else:
+            raise ValueError(
+                'x must be a positive number in (0, modulus). Found {}'.format(
+                    x))
+        self._seeds[self._stream] = int(x)
+
+    def stream(self, stream_id):
         """
         Selects the current stream.
-        :param stream: stream index in [0,STREAMS-1]
+        :param stream_id: stream index in [0,STREAMS-1]
         """
-        select_stream(stream)
+        self._stream = stream_id % self._streams
+        if self._init is False and self._stream != 0:
+            self.plant_seeds(self._iseed)
 
     def rnd(self):
         """
         Generates a pseudo-random number from uniform distribution in [0,1)
         :return: a uniform pseudo-random float in [0,1)
         """
-        u = rnd()
-        return u
+        Q = int(self._modulus / self._multiplier)
+        R = int(self._modulus % self._multiplier)
+
+        t = int(self._multiplier * (self._seeds[self._stream] % Q) -
+                R * int(self._seeds[self._stream] / Q))
+        if t > 0:
+            self._seeds[stream] = int(t)
+        else:
+            self._seeds[stream] = int(t + self._modulus)
+
+        return float(self._seeds[self._stream] / self._modulus)
 
     def rndn(self, n):
         """
-        Generates n pseudo-random numbers from uniform distribution in [0,1)
-        :return: a list of uniform pseudo-random floats in [0,1)
+        Generates a list of pseudo-random numbers from uniform distribution in
+        [0,1)
+        :param n: (int) number of random numbers to generate
+        :return: (list(float)) a list of uniform pseudo-random numbers.
         """
         sample = []
         for i in range(0, n):
-            u = rnd()
+            u = self.rnd()
             sample.append(u)
         return sample
-
-
-def rnd():
-    """
-    Generates a pseudo-random number from uniform distribution in [0,1).
-    :return: (float) a uniform pseudo-random number in [0,1).
-    """
-    global seed
-
-    Q = int(_MODULUS / _MULTIPLIER)
-    R = int(_MODULUS % _MULTIPLIER)
-
-    t = int(_MULTIPLIER * (seed[stream] % Q) - R * int(seed[stream] / Q))
-    if t > 0:
-        seed[stream] = int(t)
-    else:
-        seed[stream] = int(t + _MODULUS)
-
-    return float(seed[stream] / _MODULUS)
-
-
-def select_stream(sid):
-    """
-    Selects the current stream.
-    :param sid: stream index in [0,STREAMS-1].
-    """
-    global stream
-    stream = sid % _STREAMS
-    if init is False and stream != 0:
-        plant_seeds(_ISEED)
-
-
-def plant_seeds(x):
-    """
-    Initializes all the streams of the generator.
-    :param x: (int) the initial seed.
-    """
-
-    global init
-    global stream
-    global seed
-
-    Q = int(_MODULUS / _JUMPER)
-    R = int(_MODULUS % _JUMPER)
-
-    init = True
-    s = stream
-    select_stream(0)
-    put_seed(x)
-    stream = s
-    for j in range(1, _STREAMS):
-        x = int(_JUMPER * (seed[j - 1] % Q) - R * int((seed[j - 1] / Q)))
-        if x > 0:
-            seed[j] = x
-        else:
-            seed[j] = x + _MODULUS
-
-
-def put_seed(x):
-    """
-    Initializes the current stream with the specified seed.
-    :param x: (int) the seed.
-    """
-    global seed
-    if x > 0:
-        x %= _MODULUS
-    seed[stream] = int(x)
-
-
-def get_seed():
-    """
-    Retrieves the seed of the current stream.
-    :return: (int) the seed of the current stream.
-    """
-    return seed[stream]
